78ce64c2342fd3cb960ddb07aa17ba5eb20b32a7
Lucas Lopes <128646570+LucasPTe@users.noreply.github.com>
2024-09-06 14:33:28 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AppAndre',
+  webDir: 'www'
+};
+
+export default config;
