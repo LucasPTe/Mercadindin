6d1ab93a5b4b9ce0e10d4902d2bb6f9020fc972b
KBezerra <147563397+KBezerra@users.noreply.github.com>
2024-11-24 00:20:48 -0300

    Paginas dos Devs feita
    Paginas dos devs feita.

--- src/app/desenvolvedores/desenvolvedores.page.ts
+++ src/app/desenvolvedores/desenvolvedores.page.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-desenvolvedores',
   templateUrl: './desenvolvedores.page.html',
   styleUrls: ['./desenvolvedores.page.scss'],
 })
-export class DesenvolvedoresPage implements OnInit {
+export class DesenvolvedoresPage {
+  developers = [
+    {
+      name: 'Kauã da Silva Bezerra',
+      photo: 'assets/images/img_devs/Kaua.jpeg', // Substitua pelo caminho real da imagem
+      instagramUsername: 'okaua_silva',
+      githubUsername: 'KBezerra',
+      linkedinUsername: 'Kauã Silva',
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    },
+    {
+      name: 'Annibal Gulias',
+      photo: 'assets/annibal.jpg', // Substitua pelo caminho real da imagem
+      instagramUsername: '@annibal',
+      githubUsername: 'annibal',
+      linkedinUsername: 'annibal'
+    },
+    {
+      name: 'Lucas Roberto Lopes',
+      photo: 'assets/lucas.jpg', // Substitua pelo caminho real da imagem
+      instagramUsername: '@lucas',
+      githubUsername: 'lucas',
+      linkedinUsername: 'lucas'
+    },
+    {
+      name: 'Caio Nery',
+      photo: 'assets/caio.jpg', // Substitua pelo caminho real da imagem
+      instagramUsername: '@caio',
+      githubUsername: 'caio',
+      linkedinUsername: 'caio'
+    }
+  ];
 }
